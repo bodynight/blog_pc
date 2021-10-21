cee0ef9bd4ae67528f7b232006bafaf09d940ae1
bodynight <xbodynightx@gmail.com>
2021-10-21 22:30:57 +0300

    update visito_spec

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 
 feature "Contact creation" do
-  scenario "allows acees to contact page" do
+  scenario "allows accees to contact page" do
     visit '/contacts'
 
     expect(page).to have_content I18n.t('contacts.contact_us')
+  end
 
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Message', with: 'caplin'
+    click_button 'Send Message'
+
+    expect(page).to have_content 'Thanks!'
   end
 end
