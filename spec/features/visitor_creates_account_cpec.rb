dcb117bb2d53e900980a7a4849d9d7426de668fa
bodynight <xbodynightx@gmail.com>
2021-10-22 18:02:21 +0300

    create visitor sign up spec

---
+++ spec/features/visitor_creates_account_cpec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "Account creation" do
+  scenario "allows guest create account" do
+    visit new_user_registration_path
+
+    fill_in :user_email, :with 'usr@hjgjhghkrt'
+    fill_in :user_username, :with 'blablart'
+    fill_in :user_password, :with '12345678'
+    fill_in :user_password_confirmation, :with '12345678'
+
+    clic_button 'Sign up'
+
+    expect(page).to have_content I18n.t('devise.registrations.signed_up')
+  end
+end
