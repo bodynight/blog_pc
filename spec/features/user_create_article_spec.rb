c9c15cd364148be4db57e8d456826ff55843440f
bodynight <xbodynightx@gmail.com>
2021-10-24 10:16:09 +0300

    update user create article spec

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -9,7 +9,16 @@ feature "Article Creation" do
 
     scenario "allows user to visit new article page" do
       visit  new_article_path
-      expect(page).to have_content 'New articles'
+      expect(page).to have_content I18n.t('articles.new.new_articles')
+    end
+
+    scenario "allows user to visit new article page" do
+      visit  new_article_path
+      fill_in 'article_title', with: 'Name Article'
+      fill_in 'article_text', with: 'Blabla'
+      click_button 'Save Article'
+
+      expect(page).to have_content 'Name Article'
     end
 
 
