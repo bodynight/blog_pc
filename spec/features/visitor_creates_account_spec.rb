83d7fa7035edae2298f2477467efbb63fce31d6e
bodynight <xbodynightx@gmail.com>
2021-10-24 09:28:12 +0300

    update

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 feature "Account creation" do
+
   scenario "allows guest create account" do
     sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
+
+
 end
 
 
